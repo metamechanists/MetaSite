ab65e97c168d7464217944546717968c8bd68f22
MetaMechanists <35176119+LordIdra@users.noreply.github.com>
2023-03-30 13:54:30 +0100

    -

--- components/documentation-menu.js
+++ components/documentation-menu.js
@@ -17,14 +17,109 @@ class DocumentationMenu extends HTMLElement {
               <h class="main-menu-section-title">MODERATION</h>
 
               <ul class="main-menu-list main-menu-list-item main-menu-icon main-menu-text menu-divider">
-                  <a href="server-rules">
+                  <a href="TODO">
                       <img src="image/icons/format-list-bulleted-square.svg">
                       Commands
                   </a>
 
-                  <a href="server-rules">
+                  <a href="TODO">
                       <img src="image/icons/format-list-bulleted-square.svg">
-                      Guidelines
+                      Consequences
+                  </a>
+
+                  <a href="TODO">
+                      <img src="image/icons/format-list-bulleted-square.svg">
+                      Ban Appeals
+                  </a>
+
+                  <a href="TODO">
+                      <img src="image/icons/format-list-bulleted-square.svg">
+                      Communication
+                  </a>
+
+                  <a href="TODO">
+                      <img src="image/icons/format-list-bulleted-square.svg">
+                      Governance
+                  </a>
+              </ul>
+
+              <hr class="main-menu-divider">
+              <h class="main-menu-section-title">INFRASTRUCTURE</h>
+
+              <ul class="main-menu-list main-menu-list-item main-menu-icon main-menu-text menu-divider">
+                  <a href="docs-infrastructure-overview.html">
+                      <img src="image/icons/format-list-bulleted-square.svg">
+                      Overview
+                  </a>
+
+                  <a href="TODO">
+                      <img src="image/icons/format-list-bulleted-square.svg">
+                      DNS
+                  </a>
+
+                  <a href="TODO">
+                      <img src="image/icons/format-list-bulleted-square.svg">
+                      Tunnel
+                  </a>
+
+                  <a href="TODO">
+                      <img src="image/icons/format-list-bulleted-square.svg">
+                      SSH
+                  </a>
+
+                  <a href="TODO">
+                      <img src="image/icons/format-list-bulleted-square.svg">
+                      Reverse Proxy
+                  </a>
+
+                  <a href="TODO">
+                      <img src="image/icons/format-list-bulleted-square.svg">
+                      Website
+                  </a>
+
+                  <a href="TODO">
+                      <img src="image/icons/format-list-bulleted-square.svg">
+                      Map
+                  </a>
+
+                  <a href="TODO">
+                      <img src="image/icons/format-list-bulleted-square.svg">
+                      Analytics
+                  </a>
+
+                  <a href="TODO">
+                      <img src="image/icons/format-list-bulleted-square.svg">
+                      Jenkins
+                  </a>
+
+                  <a href="TODO">
+                      <img src="image/icons/format-list-bulleted-square.svg">
+                      MySQL
+                  </a>
+
+                  <a href="TODO">
+                      <img src="image/icons/format-list-bulleted-square.svg">
+                      Postgres
+                  </a>
+
+                  <a href="TODO">
+                      <img src="image/icons/format-list-bulleted-square.svg">
+                      Synapse
+                  </a>
+
+                  <a href="TODO">
+                      <img src="image/icons/format-list-bulleted-square.svg">
+                      Coturn
+                  </a>
+
+                  <a href="TODO">
+                      <img src="image/icons/format-list-bulleted-square.svg">
+                      Bungee
+                  </a>
+
+                  <a href="TODO">
+                      <img src="image/icons/format-list-bulleted-square.svg">
+                      Cable
                   </a>
               </ul>
           </div>`
